74fb2fd625d21b9ad34200bb63e7ecd136ebe93d
Ari <1504653078@qq.com>
2023-05-28 13:18:17 +0800

    Revert "修改配置文件就可以"
    This reverts commit 6b861afd4121e0552ff38270fa0102c3397b18b0.

--- FunionBlazor.Web.Entry/Program.cs
+++ FunionBlazor.Web.Entry/Program.cs
@@ -1,3 +1,4 @@
-Serve.Run(RunOptions.Default.WithArgs(args));
-
+//Serve.Run(RunOptions.Default.WithArgs(args));
+using FunionBlazor.Web.Entry;
+Serve.Run(RunOptions.Default.AddWebComponent<MASAWebComponent>());
 
