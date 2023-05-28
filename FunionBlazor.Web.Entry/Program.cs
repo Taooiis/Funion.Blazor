6b861afd4121e0552ff38270fa0102c3397b18b0
Ari <1504653078@qq.com>
2023-05-28 13:12:28 +0800

    修改配置文件就可以

--- FunionBlazor.Web.Entry/Program.cs
+++ FunionBlazor.Web.Entry/Program.cs
@@ -1,4 +1,3 @@
-//Serve.Run(RunOptions.Default.WithArgs(args));
-using FunionBlazor.Web.Entry;
-Serve.Run(RunOptions.Default.AddWebComponent<MASAWebComponent>());
+Serve.Run(RunOptions.Default.WithArgs(args));
+
 
