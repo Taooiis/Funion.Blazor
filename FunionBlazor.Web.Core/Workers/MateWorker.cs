2add8c995d26cc2468460fbf973badd91e3a95d2
Ari <taoosii@foxmail.com>
2023-06-08 13:21:44 +0800

    修改图标

--- FunionBlazor.Web.Core/Workers/MateWorker.cs
+++ FunionBlazor.Web.Core/Workers/MateWorker.cs
@@ -14,8 +14,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-using static System.Net.Mime.MediaTypeNames;
-
 namespace FunionBlazor.Web.Core;
 public class MateWorker : BackgroundService
 {
